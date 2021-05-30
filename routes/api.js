6fcd89db90f1c6ffe85c9c53881584c2827e16cb
Malcolm Easton <eastonmalcolm@gmail.com>
2021-05-30 14:38:28 -0600

    set up basic routing

---
+++ routes/api.js
@@ -0,0 +1,15 @@
+const api = require('express').Router();
+
+api.get('/notes', (req,res,next) => {
+    
+});
+
+api.post('/notes', (req,res,next) => {
+
+});
+
+api.delete('/notes/:id', (req,res,next) => {
+
+});
+
+module.exports = api;
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+//Split out routes into separate files
+const api = require('./routes/api');
 
-app.use(express.static('public'));
+//Making all the public stuff public
+app.use(express.static('public',{extensions:['html']}));
+app.use('/api',api);
 
 app.listen(port, () => {
     console.log('App launched at port ' + port);
