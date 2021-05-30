5dce398a96e1fa63e1695118dee0af643150ff2e
Malcolm Easton <eastonmalcolm@gmail.com>
2021-05-30 14:47:07 -0600

    restructured datahandler and added to server

--- dataHandler.js
+++ dataHandler.js
@@ -1,13 +1,19 @@
+//This module is intended to handle all internal I/O between the server and the db
 const fs = require('fs').promises;
 
 module.exports = {
-    async readNotes(){
-
+    async init(){
+        
     },
-    async addNote(){
+    notes: {
+        async get(){
 
-    },
-    async deleteNote(){
-
+        },
+        async add(){
+    
+        },
+        async delete(){
+    
+        }
     }
 }
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+const dataHandler = require('./dataHandler');
+
 //Split out routes into separate files
 const api = require('./routes/api');
+
 
 //Making all the public stuff public
 app.use(express.static('public',{extensions:['html']}));
