b523c5bc469571ebdf25c751f4fb4a2fb7a983d3
Malcolm Easton <eastonmalcolm@gmail.com>
2021-05-30 21:41:39 -0600

    populated datahandling functions

--- dataHandler.js
+++ dataHandler.js
@@ -1,19 +1,34 @@
 //This module is intended to handle all internal I/O between the server and the db
 const fs = require('fs').promises;
 
+const dbPath = './db/db.json';
+let notesData = [];
+
+function saveNotes(){
+    return fs.writeFile(dbPath,JSON.stringify(notesData)).catch(console.error);
+}
+
 module.exports = {
-    async init(){
-        
+    init(){
+        try {
+            notesData = require(dbPath);
+        } catch {
+            return saveNotes();
+        }
     },
     notes: {
-        async get(){
-
+        get(){
+            return notesData;
         },
-        async add(){
-    
+        add(note){
+            notesData.push(note);
+            return saveNotes();
         },
-        async delete(){
-    
+        delete(id){
+            const noteIndex = notesData.findIndex(note => note.id === id);
+            if(noteIndex < 0) return; //Exit early if title not found
+            notesData.splice(noteIndex, 1);
+            return saveNotes();
         }
     }
 }
