6ef97ecba53389627e4833894a6ccd033c09864c
Malcolm Easton <eastonmalcolm@gmail.com>
2021-05-30 22:53:07 -0600

    mapped requests to datahandler

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,20 @@
-const api = require('express').Router();
+const dataHandler = require('../dataHandler');
+const express = require('express');
+const api = express.Router();
+
+api.use(require('body-parser').json());
 
 api.get('/notes', (req,res,next) => {
-    
+    res.json(dataHandler.notes.get());
 });
 
 api.post('/notes', (req,res,next) => {
-
+    res.json(dataHandler.notes.add(req.body.title, req.body.text));
 });
 
 api.delete('/notes/:id', (req,res,next) => {
-
+    dataHandler.notes.delete(req.params.id);
+    res.sendStatus(200);
 });
 
 module.exports = api;
